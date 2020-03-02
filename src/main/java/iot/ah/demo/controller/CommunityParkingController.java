5cbb81b7f0c1bbfdd043d4888e7088678b52257b
袁继康 <jikang.yuan@bangdao-tech.com>
2020-03-02 15:09:42 +0800

    iot测试

--- src/main/java/iot/ah/demo/controller/CommunityParkingController.java
+++ src/main/java/iot/ah/demo/controller/CommunityParkingController.java
@@ -29,7 +29,7 @@ import java.util.Map;
  */
 @RestController
 @CrossOrigin
-@RequestMapping(value = "/CommunityParkingService",consumes = MediaType.APPLICATION_JSON_VALUE,method = RequestMethod.POST)
+@RequestMapping(value = "/CommunityParkingService")
 public class CommunityParkingController {
     @Autowired
     private Environment env;
