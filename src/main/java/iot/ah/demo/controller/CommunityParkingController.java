00b41a4682459e76b6bb13c1b7ebd08c8450775c
袁继康 <jikang.yuan@bangdao-tech.com>
2020-03-02 12:36:44 +0800

    iot测试

--- src/main/java/iot/ah/demo/controller/CommunityParkingController.java
+++ src/main/java/iot/ah/demo/controller/CommunityParkingController.java
@@ -1,12 +1,27 @@
 package iot.ah.demo.controller;
 
+import com.alibaba.cloudapi.sdk.model.ApiResponse;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.aliyun.iotx.api.client.IoTApiClientBuilderParams;
+import com.aliyun.iotx.api.client.IoTApiRequest;
+import com.aliyun.iotx.api.client.SyncApiClient;
 import iot.ah.demo.core.mysql.BaseDownResponse;
 import iot.ah.demo.core.mysql.BillingRules;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,14 +34,82 @@ public class CommunityParkingController {
     @Autowired
     private Environment env;
 
-    @RequestMapping(value = "/BillingRules")
-    public BaseDownResponse getEnvProp(@RequestBody BillingRules billingRules) {
-        System.out.println(billingRules);
-        BaseDownResponse<String> stringBaseDownResponse = new BaseDownResponse<>();
-        stringBaseDownResponse.setData("success");
-        return stringBaseDownResponse;
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+    /**
+     * 系统环境变量中获取的
+     */
+    public static final String appKey = System.getenv("iot.hosting.appKey");
+    public static final String AppSecret = System.getenv("iot.hosting.appSecret");
+    //服务模型请求的路由
+    private static final String SERVICE_EDGE__PATH = System.getenv("iot.hosting.mesh.domain");
+
+    @RequestMapping("/test")
+    public String servicewk(@RequestBody Map<String, Object> params) throws UnsupportedEncodingException {
+        //服务调用的接口---一般格式为：  /服务名称/接口名称
+        String path = String.valueOf(params.get("service_path"));
+        String content = handleservice(params, path);
+        JSONObject jsonObject = JSON.parseObject(content);
+        Integer code = (Integer) jsonObject.get("code");
+        if (200 != code) {
+            return String.valueOf(jsonObject);
+        }
+        return null;
     }
 
+    private String handleservice(Map<String, Object> params, String path) throws UnsupportedEncodingException {
+        IoTApiClientBuilderParams ioTApiClientBuilderParams =
+                new IoTApiClientBuilderParams();
+        ioTApiClientBuilderParams.setAppKey(appKey);
+        ioTApiClientBuilderParams.setAppSecret(AppSecret);
+        SyncApiClient syncClient = new SyncApiClient(ioTApiClientBuilderParams);
+        //均为可调节的参数，需要自行填入
+        IoTApiRequest request = new IoTApiRequest();
+        String ApiVer = String.valueOf(params.get("ApiVer"));
+        String Version = String.valueOf(params.get("Version"));
+        request.putParam("lineName","2313213");
+        request.setApiVer(ApiVer);
+        request.setVersion(Version);
+        logger.warn("request: {}" + JSON.toJSONString(request));
+        ApiResponse response = syncClient.postBody(SERVICE_EDGE__PATH, path, request, true);
+        String content = new String(response.getBody());
+        return content;
+    }
+
+    @RequestMapping(value = "/BillingRules")
+    public BaseDownResponse getEnvProp(HttpServletRequest request) throws Exception {
+        Map<String, String> map = new HashMap<String, String>();
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+            String sign = map.get("x-ca-signature");
+            logger.info(sign);
+        }
+        String json = "";
+        String signHeaders = request.getHeader("HEADER_SM_ROUTER_DESTINATION");
+        logger.info(signHeaders);
+
+        json = new String(readInputStream(request.getInputStream()), "UTF-8");
+        logger.info("调用成功"+"请求参数"+json);
+        BaseDownResponse baseDownResponse = new BaseDownResponse();
+        baseDownResponse.setMessage("success");
+        return baseDownResponse;
+    }
+    public static byte[] readInputStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        boolean var3 = false;
+
+        int len;
+        while ((len = inStream.read(buffer)) != -1) {
+            outSteam.write(buffer, 0, len);
+        }
+
+        outSteam.close();
+        inStream.close();
+        return outSteam.toByteArray();
+    }
     @RequestMapping(value = "/getAllEnvProp")
     public Map<String, String> getEnvProp() {
         return System.getenv();
---
+++ src/main/java/iot/ah/demo/controller/HelloController.java
@@ -0,0 +1,143 @@
+package iot.ah.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.UUID;
+
+import com.alibaba.cloudapi.sdk.model.ApiResponse;
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import com.aliyun.iotx.api.client.IoTApiClientBuilderParams;
+import com.aliyun.iotx.api.client.IoTApiRequest;
+import com.aliyun.iotx.api.client.SyncApiClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloController {
+
+     static final String appKey = System.getenv("iot.hosting.appKey");
+     static final String appSecret = System.getenv("iot.hosting.appSecret");
+     static final String host = System.getenv("iot.hosting.api.domain");
+
+    String modelId = "FaceSet";
+
+    @GetMapping("/")
+    public String main(Model model) {
+        return "welcome";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert() throws UnsupportedEncodingException {
+        IoTApiClientBuilderParams ioTApiClientBuilderParams = new IoTApiClientBuilderParams();
+        ioTApiClientBuilderParams.setAppKey(appKey);
+        ioTApiClientBuilderParams.setAppSecret(appSecret);
+        SyncApiClient syncClient = new SyncApiClient(ioTApiClientBuilderParams);
+
+        IoTApiRequest request = new IoTApiRequest();
+        //设置api的版本
+        request.setApiVer("0.0.3");
+        request.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+        //如果需要登陆，设置当前的会话的token
+        //request.setIotToken("xxxxxxxxx");
+
+        request.putParam("modelId", modelId);
+
+        long now = System.currentTimeMillis();
+        String modeljson = "{'FaceSetID' : 1 ,'Name' : 'testname' ,'Image' : 'testimages'}";
+        request.putParam("properties", JSON.parse(modeljson));
+        request.setVersion("1.0");// 网关协议版本
+        System.out.println(request);
+        ApiResponse response = syncClient.postBody(host, "/data/model/data/insert", request, false);
+        System.out.println("response code = " + response.getCode() + " response = " + new String(response.getBody(), "UTF-8"));
+        return new String(response.getBody(), "UTF-8");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete() throws UnsupportedEncodingException {
+        IoTApiClientBuilderParams ioTApiClientBuilderParams = new IoTApiClientBuilderParams();
+        ioTApiClientBuilderParams.setAppKey(appKey);
+        ioTApiClientBuilderParams.setAppSecret(appSecret);
+        SyncApiClient syncClient = new SyncApiClient(ioTApiClientBuilderParams);
+
+        IoTApiRequest request = new IoTApiRequest();
+        //设置api的版本
+        request.setApiVer("0.0.2");
+        request.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+        //如果需要登陆，设置当前的会话的token
+        //request.setIotToken("xxxxxxxxx");
+
+        request.putParam("modelId", modelId);
+        String condition = ("[{'fieldName' : 'id' , 'value' : 5 , 'operate' : 'eq'}]");
+        request.putParam("conditions", JSON.parseArray(condition));
+        request.setVersion("1.0");// 网关协议版本
+        System.out.println(request);
+        ApiResponse response = syncClient.postBody(host,
+                "/data/model/data/delete", request, false);
+        System.out.println("response code = " + response.getCode() + " response = " + new String(response.getBody(), "UTF-8"));
+        return new String(response.getBody(), "UTF-8");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "query", method = RequestMethod.POST)
+    public String query() throws UnsupportedEncodingException {
+        IoTApiClientBuilderParams ioTApiClientBuilderParams = new IoTApiClientBuilderParams();
+        ioTApiClientBuilderParams.setAppKey(appKey);
+        ioTApiClientBuilderParams.setAppSecret(appSecret);
+        SyncApiClient syncClient = new SyncApiClient(ioTApiClientBuilderParams);
+
+        IoTApiRequest request = new IoTApiRequest();
+        //设置api的版本
+        request.setApiVer("0.0.3");
+        request.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+        //如果需要登陆，设置当前的会话的token
+        //request.setIotToken("xxxxxxxxx");
+
+        request.putParam("modelId", modelId);
+        //request.putParam("scopeId", scopeId);
+        //String condition = ("[{'fieldName' : 'id' , 'value' : 4 , 'operate' : 'eq'}]");
+        request.putParam("conditions", JSON.parseArray("[]"));
+        List<String> returnFields = Lists.newArrayList("*");
+        request.putParam("returnFields", returnFields);
+        request.setVersion("1.0");// 网关协议版本
+        System.out.println(request);
+        ApiResponse response = syncClient.postBody(host,
+                "/data/model/data/query", request, false);
+        System.out.println("response code = " + response.getCode() + " response = " + new String(response.getBody(), "UTF-8"));
+        return new String(response.getBody(), "UTF-8");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public String update() throws UnsupportedEncodingException {
+        IoTApiClientBuilderParams ioTApiClientBuilderParams = new IoTApiClientBuilderParams();
+        ioTApiClientBuilderParams.setAppKey(appKey);
+        ioTApiClientBuilderParams.setAppSecret(appSecret);
+        SyncApiClient syncClient = new SyncApiClient(ioTApiClientBuilderParams);
+
+        IoTApiRequest request = new IoTApiRequest();
+        //设置api的版本
+        request.setApiVer("0.0.2");
+        request.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+        //如果需要登陆，设置当前的会话的token
+        //request.setIotToken("xxxxxxxxx");
+
+        request.putParam("modelId", modelId);
+        String condition = ("[{'fieldName' : 'id' , 'value' : 4 , 'operate' : 'eq'}]");
+        request.putParam("updateDetails", JSON.parse("{'serialNumber' : '22' , 'photoID' : '22'}"));
+        request.setVersion("1.0");// 网关协议版本
+        System.out.println(request);
+        ApiResponse response = syncClient.postBody(host,
+                "/data/model/data/update", request, false);
+        System.out.println("response code = " + response.getCode() + " response = " + new String(response.getBody(), "UTF-8"));
+        return new String(response.getBody(), "UTF-8");
+    }
+}
